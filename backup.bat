b925f08e386027c4e839947055ac9765075d7f3a
LinuxPlayer <40536440+linux-player9@users.noreply.github.com>
2025-05-03 00:07:22 +0200

    Update backup.bat

--- backup.bat
+++ backup.bat
@@ -1,13 +1,13 @@
 :: demande d'être copié à l'emplacement des données et se ferme.
-if NOT EXIST "OblivionRemastered\Content\Dev\ObvData\Data\Plugins.txt" (
+if NOT EXIST "OblivionRemastered\Content\Dev\ObvData\Data" (
 msg * copiez le script a cote du dossier OblivionRemastered
-exit
+exit /b
 )
 :: Vérifie si le .bat est lancé à côté du dossier OblivionRemastered
-if EXIST "OblivionRemastered\Content\Dev\ObvData\Data\Plugins.txt" (
+if EXIST "OblivionRemastered\Content\Dev\ObvData\Data" (
 cd "OblivionRemastered\Content\Dev\ObvData\Data"
 )
-:: Renomme les fichiers en .old
+:: Renomme les fichiers originaux en .old
 move "AltarDeluxe.bsa" "AltarDeluxe.bsa".old
 move "AltarESPMain.bsa" "AltarESPMain.bsa".old
 move "DLCBattlehornCastle.bsa" "DLCBattlehornCastle.bsa".old
@@ -22,3 +22,4 @@ move "Oblivion - Voices1.bsa" "Oblivion - Voices1.bsa".old
 move "Oblivion - Voices2.bsa" "Oblivion - Voices2.bsa".old
 :: informe que la copie peut se faire
 msg * vous pouvez maintenant fusionner OblivionRemastered du mod avec OblivionRemastered du jeu
+exit /b
